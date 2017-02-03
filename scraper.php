5c1138a2b89ddb5d1b733f1d96b8e1c39469d7fa
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 14:10:13 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,6 +1,6 @@
 <?php
-require 'scrapewiki.php';
-require 'scrapewiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 $MAX_ID = 3;
 
 for($id = 1; $i <= $MAX_ID; $id++)
